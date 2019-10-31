7aa5ba26bd3ec3fa3fe615c31aa8511c8e385b95
Lina-ben <57159371+Lina-ben@users.noreply.github.com>
2019-10-31 13:52:28 +0100

    Update State.h

--- UTM/src/State.h
+++ UTM/src/State.h
@@ -21,7 +21,19 @@ public:
 	void insertTransition(string initial_state, char input_symbol, string next_state,  char output_symbol,
 			 char direction) {
 		//TODO inserer une transition dans le map transitions.
+		
+		if(!hasInputSymbol(input_symbol))
+		 {
+			 transitions[input_symbol].setInitialState(initial_state) ;
+			 transitions[input_symbol].setInputSymbol(input_symbol)) ;
+			 transitions[input_symbol].setNextState(next_state) ;
+			 transitions[input_symbol].setOutputSymbol(output_symbol)) ;
+			 transitions[input_symbol].setDirection(direction) ;
+		}
+
+            else throw "ERROR : we are in a deterministic turing machine, there can only be one transition for a particular state and input";
 	}
+	
 
 	bool hasInputSymbol(char input_symbol) {
 		//TODO Check si le symbol en entrer est deja dans la map de transition.
@@ -32,11 +44,18 @@ public:
 	}
 
 	string transitioning(Tape& tape) {
-		//TODO transitionner de l'etat actuel au nextstate, modifier la tape et déplacer l'entete
-		// si aucune transition dans la liste des transitions n'est trouvé, reject
-		return "";
+		//TODO transitionner de l'etat actuel au nextstate, modifier la tape et dÃ©placer l'entete
+		// si aucune transition dans la liste des transitions n'est trouvÃ©, reject
+		 auto it = transitions.find(tape.getHeadInput());
+				 if (it != transitions.end())
+				 {
+			             tape.setHeadInput(it->second.getOutputSymbol());
+				     tape.moveHead(it->second.getDirection());
 
-	}
+				     return it->second.getNextState()
+				 }
+                                else throw "ERROR : no transition found " ;
+             }
 
 	bool isFinalState() const {
 		return final_state;
