5d4edb5b1263b04cb2d940c0101d8af38ec0ceef
root <wacim29@hotmail.com>
2019-12-07 08:17:35 -0800

    added report

--- UTM/build.sh
+++ UTM/build.sh
@@ -1,3 +1,3 @@
-g++ -o UTM.exe  ./src/TuringMachine.h ./src/UTM.cpp ./src/Transition.h ./src/Tape.h ./src/State.h 
+g++ -o UTM  ./src/TuringMachine.h ./src/UTM.cpp ./src/Transition.h ./src/Tape.h ./src/State.h 
 
 
